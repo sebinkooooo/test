701b6754a84275894d4c6941b1b3ff56ac089aa6
sebinkooooo <biz.sebkot@gmail.com>
2025-10-17 00:54:45 +0100

    fin

--- src/main.cu
+++ src/main.cu
@@ -181,7 +181,7 @@ void run_crt_benchmark_32(int M, const cpp_int& N,
         int blocks = (M + threads - 1) / threads;
         // CRITICAL: Use same offset as CPU (100M) to avoid CRT prime collisions
         const int START_OFFSET = 100000000;
-        u32 offset_seed = BASE_SEED_32 + (u32)((uint64_t)START_OFFSET * 104729ull);
+        u32 offset_seed = (u32)(((uint64_t)START_OFFSET * 104729ull + BASE_SEED_32));
         generate_divisors_kernel_32<<<blocks, threads>>>(offset_seed, M, d_P);
         CUDA_CHECK(cudaDeviceSynchronize());
     } else {
@@ -284,7 +284,7 @@ void run_crt_benchmark_64(int M, const cpp_int& N,
         int blocks = (M + threads - 1) / threads;
         // CRITICAL: Use same offset as CPU (100M) to avoid CRT prime collisions
         const int START_OFFSET = 100000000;
-        u64 offset_seed = BASE_SEED_64 + (u64)START_OFFSET * 104729ull;
+        u64 offset_seed = (u64)START_OFFSET * 104729ull + BASE_SEED_64;
         generate_divisors_kernel_64<<<blocks, threads>>>(offset_seed, M, d_P);
         CUDA_CHECK(cudaDeviceSynchronize());
     } else {
